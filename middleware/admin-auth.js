9c813ecb044186ac18516791e556022ed8303e07
alexkurz96 <akex-kurz@mail.ru>
2020-03-30 16:17:37 +0300

    add server

--- middleware/admin-auth.js
+++ middleware/admin-auth.js
@@ -2,4 +2,4 @@ export default function({store, redirect}) {
   if (!store.getters['auth/isAuthenticated']) {
     redirect('/admin/login?message=login');
   }
-};
+}
---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const passportSrategy = require('./middelware/passport-srategy');
+const authRoutes = require('./routes/auth.routes');
+const postRoutes = require('./routes/post.routes');
+const commentRoutes = require('./routes/comment.routes');
+const keys = require('./keys');
+const app = express();
+
+mongoose.connect(keys.MONGO_URI,  {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
+  .then(() => console.log('MongoDB connected...'))
+  .catch(error => console.error(error));
+
+app.use(passport.initialize());
+passport.use(passportSrategy);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+
+module.exports = app;
