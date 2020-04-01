4021b8fdd76ad063c12c2190f487c430f95cb02d
alexkurz96 <33290880+alexkurz96@users.noreply.github.com>
2020-04-01 12:01:49 +0300

    Delete keys.dev.js

--- server/keys/keys.dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-//mongo alex TuqZE5byvt0Gtk8S
-  MONGO_URI: `mongodb+srv://alex:TuqZE5byvt0Gtk8S@nuxt-blog-iojmd.gcp.mongodb.net/test?retryWrites=true&w=majority`,
-  JWT: 'dev-jwt-key'
-};
